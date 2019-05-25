1d30a7d35ba01870f49d3273cc845fd36facffc9
Tropical <42101043+Tropix126@users.noreply.github.com>
2019-05-25 14:22:36 -0500

    okay does json support *?

--- particles.js
+++ particles.js
@@ -69,7 +69,7 @@ particlesJS("particles-js", {
     }
   },
   "interactivity": {
-    "detect_on": "div, h1, h2, p, center, a, body, html, head, canvas",
+    "detect_on": "*",
     "events": {
       "onhover": {
         "enable": true,
